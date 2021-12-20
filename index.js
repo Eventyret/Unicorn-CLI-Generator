a63bdabc6cb7b2ffcb324f3c53baac26c44c6513
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-20 17:32:19 +0000

    📦 NEW: Added ability to create a cli with templates

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+const path = require('path');
+const copy = require('copy-template-dir');
+
+const vars = { name: `cli-img`, description: `CLI to resize and optimize images`, version: '0.0.1' };
+
+const inDir = path.join(__dirname, `template`);
+const outDir = path.join(process.cwd(), vars.name);
+
+copy(inDir, outDir, vars, (err, createdFiles) => {
+  if (err) throw err;
+  console.log();
+  console.log(`Creating files in : ./${vars.name}`);
+  createdFiles.forEach((filePath) => {
+    const fileName = path.basename(filePath);
+    console.log(`Created ${fileName}`);
+  });
+  console.log('Done');
+  console.log();
+});
