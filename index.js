f21ac38471d5642b35346d0fb8d7c7e0ae6aaac3
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-20 17:40:14 +0000

    📦 NEW: User can now give a name for the cli

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
 const path = require('path');
 const copy = require('copy-template-dir');
+const { Input } = require('enquirer');
+const init = require('./utiils/init');
+const to = require('await-to-js').default;
+const handleError = require('cli-handle-error');
 
-const vars = { name: `cli-img`, description: `CLI to resize and optimize images`, version: '0.0.1' };
+async () => {
+  init();
 
-const inDir = path.join(__dirname, `template`);
-const outDir = path.join(process.cwd(), vars.name);
+  const [err, name] = await to(new Input({ message: `CLI nanme>`, hint: `(use kebab-case only)` }).run());
 
-copy(inDir, outDir, vars, (err, createdFiles) => {
-  if (err) throw err;
-  console.log();
-  console.log(`Creating files in : ./${vars.name}`);
-  createdFiles.forEach((filePath) => {
-    const fileName = path.basename(filePath);
-    console.log(`Created ${fileName}`);
+  handleError('INPUT', err);
+  const vars = { name, description: `CLI to resize and optimize images`, version: '0.0.1' };
+
+  const inDir = path.join(__dirname, `template`);
+  const outDir = path.join(process.cwd(), vars.name);
+
+  copy(inDir, outDir, vars, (err, createdFiles) => {
+    if (err) throw err;
+    console.log();
+    console.log(`Creating files in : ./${vars.name}`);
+    createdFiles.forEach((filePath) => {
+      const fileName = path.basename(filePath);
+      console.log(`Created ${fileName}`);
+    });
+    console.log('Done');
+    console.log();
   });
-  console.log('Done');
-  console.log();
-});
+};
--- utils/init.js
+++ utils/init.js
@@ -1,7 +1,8 @@
 const welcome = require('cli-welcome');
 const pkg = require('./../package.json');
-
+const unhandled = require('cli-handle-unhandled');
 module.exports = () => {
+  unhandled();
   welcome({
     title: 'create-unicorn-cli',
     tagLine: 'by eventyret (dehlin.dev)',
