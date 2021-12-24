f777e971433c239728dd55624389da5caca1453f
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-24 13:34:43 +0000

    👌 IMPROVE: Improved messages when completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const copy = require('copy-template-dir');
 const { green: g, dim: d } = require('chalk');
+const alert = require('cli-alert');
 const init = require('./utils/init');
 const ask = require('./utils/ask');
 
@@ -18,19 +19,21 @@ const ask = require('./utils/ask');
   const license = await ask({ message: `License` });
 
   const vars = { name, description, version, authorName, authorEmail, authorURL };
+  const outDir = vars.name;
+  const inDirPath = path.join(__dirname, `template`);
+  const outDirPath = path.join(process.cwd(), outDir);
 
-  const inDir = path.join(__dirname, `template`);
-  const outDir = path.join(process.cwd(), vars.name);
-
-  copy(inDir, outDir, vars, (err, createdFiles) => {
+  copy(inDirPath, outDirPath, vars, (err, createdFiles) => {
     if (err) throw err;
-    console.log();
-    console.log(d(`Creating files in : ${g(`./${vars.name}`)}`));
+    console.log(d(`\nCreating files in : ${g(`./${outDir}`)}`));
     createdFiles.forEach((filePath) => {
       const fileName = path.basename(filePath);
       console.log(`Created ${fileName}`);
     });
-    console.log('Done');
-    console.log();
+    alert({
+      type: 'success',
+      name: 'ALL DONE',
+      msg: `\n\n${createdFiles.length} files created in ${d(`./${outDir}`)} directory`,
+    });
   });
 })();
