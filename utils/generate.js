0f7bda190fcb596ab3141177741b10753d778bc1
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-26 15:25:41 +0000

    👌 IMPROVE: Added spinners with iunfo when creating depenencies

--- utils/generate.js
+++ utils/generate.js
@@ -1,13 +1,14 @@
 const path = require('path');
-const copy = require('copy-template-dir');
-const { green: g, dim: d } = require('chalk');
-const alert = require('cli-alerts');
-const questions = require('./questions');
+const ora = require('ora');
 const execa = require('execa');
+const copy = require('copy-template-dir');
+const { green: g, dim: d, yellow: y } = require('chalk');
+const alert = require('cli-alerts');
 
+const spinner = ora({ text: `` });
+const questions = require('./questions');
 module.exports = async () => {
   const vars = await questions();
-
   const outDir = vars.name;
   const inDirPath = path.join(__dirname, `../template`);
   const outDirPath = path.join(process.cwd(), outDir);
@@ -15,17 +16,36 @@ module.exports = async () => {
   copy(inDirPath, outDirPath, vars, async (err, createdFiles) => {
     if (err) throw err;
     console.log(d(`\nCreating files in : ${g(`./${outDir} directory:\n`)}`));
-    createdFiles.forEach((filePath) => {
+    createdFiles.forEach(filePath => {
       const fileName = path.basename(filePath);
       console.log(`${g(`CREATED`)} Created ${fileName}`);
     });
+    console.log();
+    spinner.start(
+      `${y(`DEPENDENCIES`)} installing...\n\n${d(
+        `This may take a while... grab a coffee ☕️`
+      )}`
+    );
     process.chdir(outDirPath);
+    const pkgs = [
+      `meow`,
+      `chalk`,
+      `cli-alerts`,
+      `cli-welcome`,
+      `cli-meow-help`,
+      `cli-handle-error`,
+      `cli-handle-unhandled`
+    ];
+    await execa(`npm`, [`install`, ...pkgs]);
     await execa(`npm`, [`dedupe`]);
+    spinner.succeed(`${g(`DE{ENDENCIES}`)} installed!`);
 
     alert({
       type: 'success',
       name: 'ALL DONE',
-      msg: `\n\n${createdFiles.length} files created in ${d(`./${outDir}`)} directory`,
+      msg: `\n\n${createdFiles.length} files created in ${d(
+        `./${outDir}`
+      )} directory`
     });
   });
 };
