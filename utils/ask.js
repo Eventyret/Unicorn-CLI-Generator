55b88432b8048af83c817ad3f8a7a0935d5ecea6
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-26 15:46:54 +0000

    📦 NEW: Added History to persists

--- utils/ask.js
+++ utils/ask.js
@@ -1,16 +1,33 @@
 const { Input } = require('enquirer');
 const fs = require('fs');
+const path = require('path');
 const to = require('await-to-js').default;
 const handleError = require('cli-handle-error');
 const shouldCancel = require('cli-should-cancel');
+const { Store } = require('data-store');
 
 module.exports = async ({ name, message, hint, initial }) => {
+  let history = false;
+  if (
+    !initial &&
+    name !== `name` &&
+    name !== `command` &&
+    name !== `description`
+  ) {
+    history = {
+      autosave: true,
+      store: new Store({
+        path: path.join(__dirname, `/../.history/${name}.json`)
+      })
+    };
+  }
   const [err, response] = await to(
     new Input({
       name,
       message,
       hint,
       initial,
+      history,
       validate(value, state) {
         if (state && state.name === 'command') return true;
         if (state && state.name === `name`) {
